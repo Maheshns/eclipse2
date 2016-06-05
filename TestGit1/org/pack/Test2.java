d6ca40adf0d30efdddda67bbacf9d6a6ccbfa3c2
Mahesh Prasad N.S <ns.maheshprasad@gmail.com>
2016-06-05 19:06:55 +0530

    base1

---
+++ TestGit1/org/pack/Test2.java
@@ -0,0 +1,17 @@
+package org.pack;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello in main");
+		System.out.println("Hello in maindj dkjasldjlas");
+		System.out.println("Hello in maindj dkjasldjlas 123");
+		
+		
+		
+
+	}
+
+}
